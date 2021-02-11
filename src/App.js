2a5d12415bf8081d68a4897d8918e5321501fe35
Nicolas Ortega <nicolas.ortega-l@mail.escuelaing.edu.co>
2021-02-11 15:33:17 -0500

    Inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          &lt;h1&gt;TODO React App&lt;/h1&gt;
         </p>
         <a
           className="App-link"
---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Todo extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }   
+
+    render() {
+        return (  
+          //Add your code here to represent a TODO
+        );
+    }
+
+}
